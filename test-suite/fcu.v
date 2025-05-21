fc9f237b163a2e3270eb33e32fbbecd246879acf
Wassim <d.w.aitmoussa@gmail.com>
2025-05-21 16:02:05 +0200

    add similar example to wiki

--- test-suite/fcu.v
+++ test-suite/fcu.v
@@ -7,7 +7,7 @@ Set Unicoq LaTex File "fcu.tex".
 
 
 Set Unicoq Blume FCU.
-Theorem hehe : True.
+Theorem tt : True.
   evar (Y : Type).
   evar (X : Y).
   Unset Unicoq Blume FCU.
@@ -16,5 +16,10 @@ Theorem hehe : True.
   munify (fun x => X (S x)) (fun x => (S x, O)).
 Abort.
 
+
+Import EqNotations.
+Check let X := _ in fun x (a : S x = 0) (b : (S (S x) = 0)) => eq_rect (S x) X b _ a.
+Unset Unicoq Blume FCU.
+Fail Check let X := _ in fun x (a : S x = 0) (b : (S (S x) = 0)) => eq_rect (S x) X b _ a.
 
 Print Unicoq Stats.
